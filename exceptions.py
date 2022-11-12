cb2a9f66e9fcc38d2e4883cd2d0407c60077b8c5
Алексей <nox-1987@mail.ru>
2022-11-12 20:17:32 +0300

    add NotHomeWork

--- exceptions.py
+++ exceptions.py
@@ -1,4 +1,4 @@
-from homework import Bot
+from datetime import datetime
 
 
 class EndpointResponseError(Exception):
@@ -12,3 +12,16 @@ class EndpointResponseError(Exception):
             "Эндпоинт API Яндекс.Практикума недоступен: "
             f"Код ответа: {self.status_code}"
         )
+
+
+class NotHomeWorks(Exception):
+    """Ошибка вызванная отсутствием новых работ от даты current_date."""
+
+    def __init__(self, response):
+        self.current_date = response.get("current_date")
+
+    def __str__(self) -> str:
+        dt = datetime.utcfromtimestamp(self.current_date).strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+        return f"Начиная с {dt}, домашних работ нет."
