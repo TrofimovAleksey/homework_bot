1a8ba4e3d136d703c4d3140c6b07dba785437f82
Трофимов Алексей <9939713@mail.ru>
2022-11-13 20:22:34 +0300

    исправил замечания ревьювера

--- homework.py
+++ homework.py
@@ -1,4 +1,5 @@
 """Импортируем дополнительные библиотеки."""
+import json
 import logging
 import os
 import requests
@@ -23,17 +24,11 @@ RETRY_TIME = 600
 ENDPOINT = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
 HEADERS = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 
-HOMEWORK_STATUSES = {
+VERDICTS = {
     "approved": "Работа проверена: ревьюеру всё понравилось. Ура!",
     "reviewing": "Работа взята на проверку ревьюером.",
     "rejected": "Работа проверена: у ревьюера есть замечания.",
 }
-
-# Статус д.з. храним глобально
-STATUS = None
-
-# Статус отправленного исключения
-EXCEPT_MESSAGE_STATUS = None
 
 # Начало настройки логирования
 logger = logging.getLogger(__name__)
@@ -49,14 +44,14 @@ logger.addHandler(handler)
 # Конец настройки логирования
 
 
-def send_message(bot: Bot, message: str) -> None:
+def send_message(bot: Bot, message: str) -> bool:
     """Бот отправляет сообщение."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
     except Exception as error:
         logger.error(f"Ошибка при отправке сообшения: {error}")
     else:
-        logger.info(f"Бот отправил сообщение {message}")
+        return True
 
 
 def get_api_answer(current_timestamp: int) -> dict:
@@ -68,7 +63,11 @@ def get_api_answer(current_timestamp: int) -> dict:
         logging.error(f"Ошибка при запросе к основному API: {error}")
     if response.status_code != HTTPStatus.OK:
         raise exceptions.EndpointResponseError(response.status_code)
-    return response.json()
+    try:
+        response = response.json()
+    except json.decoder.JSONDecodeError:
+        logging.error("Ошибка в преобразовании ответа из JSON в Python")
+    return response
 
 
 def check_response(response: dict) -> list:
@@ -85,26 +84,21 @@ def check_response(response: dict) -> list:
 
 def parse_status(homework: dict) -> str:
     """Проверяет статус проверки домшней работы."""
-    global STATUS
-
     homework_keys = ["homework_name", "status"]
     for key in homework_keys:
         if key not in homework:
             raise KeyError(f"Ключа {key} нет в словаре homework")
+        if key == "homework_name":
+            homework_name = homework.get(key)
+        else:
+            homework_status = homework.get(key)
 
-    homework_name = homework.get("homework_name")
-    homework_status = homework.get("status")
-
-    if homework_status not in HOMEWORK_STATUSES:
+    if homework_status not in VERDICTS:
         raise ValueError(
             f"Статус {homework_status} не соответствует ожидаемому"
         )
 
-    if homework_status == STATUS:
-        return "Статус дз не изменился"
-
-    STATUS = homework_status
-    verdict = HOMEWORK_STATUSES[homework_status]
+    verdict = VERDICTS[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -130,10 +124,13 @@ def main() -> None:
     check = check_tokens()
 
     if not check:
-        sys.exit()
+        raise ValueError("Проверьте доступность переменных окружения")
 
     bot = Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
+
+    except_message_status = None
+    status = None
 
     while check:
         try:
@@ -142,8 +139,13 @@ def main() -> None:
             homeworks = check_response(response)
             if homeworks:
                 message = parse_status(homeworks[0])
-                if message != "Статус дз не изменился":
-                    send_message(bot, message)
+                if message != status:
+                    status = message
+                    flag = send_message(bot, message)
+                    if flag:
+                        logger.info(f"Бот отправил сообщение {message}")
+                else:
+                    message = "Статус дз не изменился"
             else:
                 ts = datetime.utcfromtimestamp(current_timestamp).strftime(
                     "%Y-%m-%d %H:%M:%S"
@@ -153,12 +155,13 @@ def main() -> None:
                     "обнаружено."
                 )
         except Exception as error:
-            global EXCEPT_MESSAGE_STATUS
             message = f"Сбой в работе программы: {error}"
             logger.error(message)
-            if message != EXCEPT_MESSAGE_STATUS:
-                send_message(bot, message)
-                EXCEPT_MESSAGE_STATUS = message
+            if message != except_message_status:
+                except_message_status = message
+                flag = send_message(bot, message)
+                if flag:
+                    logger.info(f"Бот отправил сообщение {message}")
             time.sleep(RETRY_TIME)
         else:
             logger.info(message)
